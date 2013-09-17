db9cb4e32800bfdbc76eee0b08244b62cfef42dd
peacekeeper <markus.sabadello@gmail.com>
2013-09-17 17:37:30 +0200

    updated sample

--- src/main/java/xdi2/example/core/ContextNodeTypesSample.java
+++ src/main/java/xdi2/example/core/ContextNodeTypesSample.java
@@ -6,8 +6,8 @@ import java.util.Iterator;
 import xdi2.core.ContextNode;
 import xdi2.core.Graph;
 import xdi2.core.features.nodetypes.XdiAbstractContext;
-import xdi2.core.features.nodetypes.XdiAttributeClass;
-import xdi2.core.features.nodetypes.XdiAttributeInstanceUnordered;
+import xdi2.core.features.nodetypes.XdiAttributeCollection;
+import xdi2.core.features.nodetypes.XdiAttributeMemberUnordered;
 import xdi2.core.impl.memory.MemoryGraphFactory;
 import xdi2.core.io.MimeType;
 import xdi2.core.io.XDIReaderRegistry;
@@ -23,9 +23,9 @@ public class ContextNodeTypesSample {
 		Graph graph = MemoryGraphFactory.getInstance().openGraph();
 		ContextNode contextNode = graph.getRootContextNode().setContextNode(XDI3SubSegment.create("=markus"));
 
-		XdiAttributeClass telAttributeClass = XdiAbstractContext.fromContextNode(contextNode).getXdiAttributeClass(XDI3SubSegment.create("+tel"), true);
-		telAttributeClass.setXdiInstanceUnordered(null).getXdiValue(true).getContextNode().setLiteral("+1.206.555.1111");
-		telAttributeClass.setXdiInstanceUnordered(null).getXdiValue(true).getContextNode().setLiteral("+1.206.555.2222");
+		XdiAttributeCollection telAttributeCollection = XdiAbstractContext.fromContextNode(contextNode).getXdiAttributeCollection(XDI3SubSegment.create("+tel"), true);
+		telAttributeCollection.setXdiMemberUnordered(null).getXdiValue(true).getContextNode().setLiteral("+1.206.555.1111");
+		telAttributeCollection.setXdiMemberUnordered(null).getXdiValue(true).getContextNode().setLiteral("+1.206.555.2222");
 
 		System.out.println(graph.toString(new MimeType("application/xdi+json;pretty=1")));
 
@@ -35,9 +35,9 @@ public class ContextNodeTypesSample {
 		XDIReaderRegistry.getAuto().read(graph2, new StringReader(graph.toString()));
 		ContextNode contextNode2 = graph.getDeepContextNode(XDI3Segment.create("=markus"));
 
-		XdiAttributeClass telCollection2 = XdiAbstractContext.fromContextNode(contextNode2).getXdiAttributeClass(XDI3SubSegment.create("+tel"), false);
+		XdiAttributeCollection telCollection2 = XdiAbstractContext.fromContextNode(contextNode2).getXdiAttributeCollection(XDI3SubSegment.create("+tel"), false);
 
-		for (Iterator<XdiAttributeInstanceUnordered> i = telCollection2.getXdiInstancesUnordered(); i.hasNext(); ) {
+		for (Iterator<XdiAttributeMemberUnordered> i = telCollection2.getXdiMembersUnordered(); i.hasNext(); ) {
 
 			System.out.println(i.next().getXdiValue(false).getContextNode().getLiteral().getLiteralData());
 		}
